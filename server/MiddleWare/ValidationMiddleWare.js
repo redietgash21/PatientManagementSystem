6345d7dee0fcc152ca9c26ee5f21aabb4988e0af
Rediet Gashaw <redietgash21@gmail.com>
2023-08-28 07:29:48 +0300

    initial commit

---
+++ server/MiddleWare/ValidationMiddleWare.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+const yup=require('yup');
+const validation=(schema)=>async(req,res,next)=>{
+  const body=req.body
+  try{
+   await schema.validate(body);
+   next();
+   return next();
+  } catch(error){
+    return res.status(400).json({error});
+  }
+
+}
+module.exports=validation;
